084081f0b2f33629299e20755a3d5dc484c4b28d
Dustin Vannatter <dustin@dustin.local>
2013-01-24 11:37:34 -0500

    initial layout for placeholder

---
+++ app/Config/config.php
@@ -0,0 +1,14 @@
+<?
+	$config['Settings'] = array(
+  	
+  		'site_mode' => 2,	// 1 = online (live), 0 = offline (maintenance), 2 = development (debug on), 9 = beta (redirect to beta login / checker)
+    	'site_url' => '',
+    	'Email' => array(
+      		'noreply' => 'noreply@kapow.biz'
+    	),
+    	'root_domain' => 'http://www.previewsworld.com',
+    	'root_domain_path' => '/Home/1/1/71/',
+    	'icon_path' => '/Users/dustin/documents/workspace/kapow.biz/app/webroot/img/covers',
+    	'icon_web_path' => '/img/covers'
+  	);
+?>
