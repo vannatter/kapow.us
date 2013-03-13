165ec635610b0d00b3180cd26854c6b5051c019a
Jon <jon@jtomlinson.net>
2013-03-13 15:43:33 -0400

    config update

--- app/Config/config.php
+++ app/Config/config.php
@@ -11,6 +11,13 @@
     	'icon_path' => '/Users/dustin/documents/workspace/kapow.biz/app/webroot/img/covers',
     	'icon_web_path' => '/img/covers',
     	'thumb_path' => '/Users/dustin/documents/workspace/kapow.biz/app/webroot/img/covers/thumbs',
-    	'thumb_web_path' => '/img/covers/thumbs'
+    	'thumb_web_path' => '/img/covers/thumbs',
+			'API' => array(
+				'google' => array(
+					'key' => 'AIzaSyClOyMOup8oRCO_g9sl82pIHePtosMA7w8',
+					'places_url' => 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s&key=%s&sensor=false&types=book_store',
+					'details_url' => 'https://maps.googleapis.com/maps/api/place/details/json?reference=%s&sensor=false&key=%s',
+				)
+			)
   	);
 ?>
