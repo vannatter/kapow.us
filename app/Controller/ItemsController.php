e3888349a5570201e87146e277dbde25d76b8ad2
Dustin Vannatter <dustin@dustin.local>
2013-04-05 20:33:27 -0400

    cleanup of login

--- app/Controller/ItemsController.php
+++ app/Controller/ItemsController.php
@@ -102,11 +102,19 @@ class ItemsController extends AppController {
 				'Item.item_date' => $release_date,
 				'Section.category_id' => $content_type
 			),
-			'limit' => 24,
+			'limit' => 1,
 			'recursive' => 4
 		);
 
 		$items = $this->paginate('Item');
+		
+		echo "<pre>";
+		print_r($items);
+		echo "</pre>";
+		
+
+echo "xxx";
+exit;
 
 		#$items = $this->Item->find('all', array('conditions' => array('Item.item_date' => $release_date, 'Section.category_id' => $content_type), 'limit' => 2500, 'recursive' => 4));
 		$categories = $this->Category->find('all', array('limit' => 2500, 'recursive' => -1));
