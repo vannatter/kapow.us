38019dec32580ae0f2fb6bca82e97533a8e967b4
Jon <jon@jtomlinson.net>
2013-04-11 17:23:28 -0400

    series item scroll list, tweaked pull list buttons

--- app/Controller/SeriesController.php
+++ app/Controller/SeriesController.php
@@ -4,10 +4,11 @@ App::uses('AppController', 'Controller');
 
 /**
  * @property Series $Series
+ * @property Item $Item
  */
 class SeriesController extends AppController {
 	public $name = 'Series';
-	public $uses = array('Series');
+	public $uses = array('Series', 'Item');
 
 	public function index() {
 	}
@@ -29,6 +30,36 @@ class SeriesController extends AppController {
 	public function viewById($id) {
 		if($series = $this->Series->findById($id)) {
 			$this->redirect(sprintf('/series/%s', parent::seoize($id, $series['Series']['series_name'])), 301);
+		}
+	}
+
+	public function items($id) {
+		if($this->request->is('ajax')) {
+			$this->layout = 'blank';
+
+			$this->Item->bindModel(array(
+				'hasOne' => array(
+					'Pull' => array(
+						'conditions' => array(
+							'Pull.user_id' => $this->Auth->user('id')
+						)
+					)
+				)
+			));
+
+			$this->paginate = array(
+				'Item' => array(
+					'limit' => 16,
+					'order' => array(
+						'Item.created' => 'DESC'
+					)
+				)
+			);
+
+			$this->Item->recursive = 0;
+			$items = $this->paginate('Item', array('Item.series_id' => $id));
+
+			$this->set('items', $items);
 		}
 	}
 }
--- app/webroot/js/page/series.js
+++ app/webroot/js/page/series.js
@@ -1,30 +1,4 @@
 $(document).ready(function() {
-	$('.toggle_favorite').on('click', function(e) {
-		e.preventDefault();
-
-		var obj = $(this);
-		var id = obj.attr('data-id');
-		var type = obj.attr('data-type');
-
-		$.getJSON('/favorites/toggle', { 'id': id, 'type': type }, function(data) {
-			if(!data.error) {
-				if(data.type == 1) {
-					if(type == 'all') {
-						$('.toggle_favorite').not('[data-type="all"]') .css('fontWeight', 'bold');
-					} else {
-						obj.css('fontWeight', 'bold');
-					}
-				} else {
-					obj.css('fontWeight', 'normal');
-				}
-			}
-		});
-
-		// return false so we don't close the menu
-		// possibly change this later
-		return false;
-	});
-
 	$('#item-scroll-list').infinitescroll({
 		loading: {
 			finishedMsg: '<div class="row"><div class="span4 offset4 infin_txt">Nothing else!</div></div>',
@@ -37,26 +11,19 @@ $(document).ready(function() {
 		debug: true,
 		animate: false,
 		path: function(nextPage) {
-			var $content = $('#content');
-			var path = $content.find('#item-scroll-nav a').last().attr("href");
+			var path = $('#item-scroll-nav a').last().attr("href");
 			if(path) {
 				var start = path.indexOf('page:');
 				var left = path.substring(0, start);
 
 				path = left + 'page:' + nextPage;
-
-				var pubid = $content.find('#ItemPublisherId').val();
-
-				if(pubid) {
-					path += '?pubid=' + pubid;
-				}
 			}
 
 			return path;
 		}
 	});
 
-	$(document).on('click', 'button.pull_list_btn', function(e) {
+	$('#series-items').on('click', 'button.pull_list_btn', function(e) {
 		e.preventDefault();
 
 		var obj = $(this);
