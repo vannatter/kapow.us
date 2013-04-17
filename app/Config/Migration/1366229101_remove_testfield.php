73fc016425129c8444f3ae147db857d1c77d6831
Jon <jon@jtomlinson.net>
2013-04-17 16:05:49 -0400

    removed migration test field

---
+++ app/Config/Migration/1366229101_remove_testfield.php
@@ -0,0 +1,54 @@
+<?php
+class RemoveTestfield extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'drop_field' => array(
+				'users' => array('testfield',),
+			),
+		),
+		'down' => array(
+			'create_field' => array(
+				'users' => array(
+					'testfield' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+				),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
