27473ed4b9c3ba2d5d026ed2f2e70d6552e4e80b
Jon <jon@jtomlinson.net>
2013-06-21 09:14:49 -0400

    added flagged item reporting

---
+++ app/Test/Fixture/FlagFixture.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * FlagFixture
+ *
+ */
+class FlagFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'status' => array('type' => 'integer', 'null' => false, 'default' => '0', 'comment' => '0=unread,1=open,99=closed'),
+		'item_type' => array('type' => 'integer', 'null' => false, 'default' => null, 'comment' => '1=item,2=series,3=creator,4=publisher,5=store'),
+		'flag_item_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'description' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'admin_user_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'user_id' => 1,
+			'status' => 1,
+			'item_type' => 1,
+			'flag_item_id' => 1,
+			'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'admin_user_id' => 1,
+			'created' => '2013-06-21 15:03:22',
+			'modified' => '2013-06-21 15:03:22'
+		),
+	);
+
+}
