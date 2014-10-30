52fd531d903dddae90f4edc136332d20203417aa
Dustin Vannatter <dustin@vannatter.com>
2014-10-30 19:03:13 -0400

    more cleanup

--- app/View/Themed/Kapow/webroot/js/common.js
+++ app/View/Themed/Kapow/webroot/js/common.js
@@ -1,3 +1,13 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-4667823-15']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_trackPageview']);
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+		
 var msg_timeout;
 var load_hot_timeout;
 
@@ -130,8 +140,6 @@ $(document).ready(function() {
 		return false;
 	});	
 	
-	
-		
 	$('.flash_pos, .flash_neg').click(function() {
 		$(this).fadeOut();
 	});
