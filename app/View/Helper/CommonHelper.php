e47d348fe534e41adbb8b81df76b196adfe95d72
Dustin Vannatter <dustin@vannatter.com>
2016-08-29 02:29:38 -0400

    repull image logic

--- app/View/Helper/CommonHelper.php
+++ app/View/Helper/CommonHelper.php
@@ -4,7 +4,7 @@ App::uses('Helper', 'View');
 
 class CommonHelper extends Helper {
 
-    public $helpers = array('Html', 'Form');
+	public $helpers = array('Html', 'Form');
 
     public function store_hours($open, $close) {
 	    $tim = "";
@@ -42,7 +42,7 @@ class CommonHelper extends Helper {
     }
 
 	public function printing($printing) {
-		switch ($printing) {			
+		switch ($printing) {
 			case "1":
 				echo "1st Printing";
 				break;
@@ -58,67 +58,33 @@ class CommonHelper extends Helper {
 			case "5":
 				echo "5th Printing";
 				break;
+			case "6":
+				echo "6th Printing";
+				break;
+			case "7":
+				echo "7th Printing";
+				break;
+			case "8":
+				echo "8th Printing";
+				break;
 		}
 	}
 
 	public function creator_thumb($orig, $which="25p") {
-
 		$ext = pathinfo($orig, PATHINFO_EXTENSION);
 		$orig = str_replace(" ", "%20", $orig);
-				
 		if ( ($ext == "jpg") || ($ext == "jpeg") ) {
-
 			if (!is_file(WWW_ROOT . $orig)) {
-
 				// original not found, we need to flag this for reload..
 				return ("/theme/Kapow/img/noprofile.png");
-
 			} else {
-
 				return ($orig);
-/*
-				// build 50p
-				$percent = 0.5;
-				$thumb_ext = '_50p.jpg';
-
-				if (!is_file(WWW_ROOT . $orig . $thumb_ext)) {
-		 			list($width, $height) = getimagesize(WWW_ROOT . $orig);
-					$new_width = $width * $percent;
-					$new_height = $height * $percent;
-					
-					$image_p = imagecreatetruecolor($new_width, $new_height);
-					$image = imagecreatefromjpeg(WWW_ROOT . $orig);
-					imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
-					
-					imagejpeg($image_p, WWW_ROOT . $orig . $thumb_ext, 100);	    
-				}
-				
-				// build 25p
-				$percent = 0.25;
-				$thumb_ext = '_25p.jpg';
-				
-				if (!is_file(WWW_ROOT . $orig . $thumb_ext)) {
-		 			list($width, $height) = getimagesize(WWW_ROOT . $orig);
-					$new_width = $width * $percent;
-					$new_height = $height * $percent;
-					
-					$image_p = imagecreatetruecolor($new_width, $new_height);
-					$image = imagecreatefromjpeg(WWW_ROOT . $orig);
-					imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
-					
-					imagejpeg($image_p, WWW_ROOT . $orig . $thumb_ext, 100);	    
-				}
-				
-				return ($orig . "_" . $which . ".jpg");
-*/
 			}
-				
-	    } else {
-	    	return $orig;
-	    }
-		
+		} else {
+			return $orig;
+		}
 	}
-	
+
 	public function thumb($orig, $which="25p") {
 		$ext = pathinfo($orig, PATHINFO_EXTENSION);
 
@@ -175,13 +141,13 @@ class CommonHelper extends Helper {
 		$clean = str_replace("'", "", $string);
 		return $id . "--" . strtolower(Inflector::slug($clean, '-'));
 	}
-    
-    public function creators($creator_array) {
-		$formatted = array();	    
+
+	public function creators($creator_array) {
+		$formatted = array();
 		$output = "";
-	    foreach ($creator_array as $c) {
-		    $formatted[$c['CreatorType']['creator_type_name']][$c['Creator']['id']] = $c['Creator']['creator_name'];
-	    }
+		foreach ($creator_array as $c) {
+			$formatted[$c['CreatorType']['creator_type_name']][$c['Creator']['id']] = $c['Creator']['creator_name'];
+		}
 
 		$output .= "<div class='creators'>";
 		foreach ($formatted as $k=>$v) {
@@ -192,24 +158,20 @@ class CommonHelper extends Helper {
 			}
 			$output .= substr($creator_string, 0, -2);
 			$output .= "</div>";
-						
 		}
-		$output .= "</div>";	    
-	    return $output;
-    }
-    
-    public function series($series_num, $series_data, $show_name_only=0) {
-		$output = "";
-	    
-	    if ( ($series_num > 0) && ($show_name_only == 0) ) {
-		    $output .= "#" . $series_num . " of ";
-	    }
+		$output .= "</div>";
+		return $output;
+	}
 
-		$output .= "<a href='/series/" . $this->seoize($series_data['id'], $series_data['series_name']) . "'>" . $series_data['series_name'] . "</a>";	    
-	    
-	    return $output;
-    }
-    
+	public function series($series_num, $series_data, $show_name_only=0) {
+		$output = "";
+		if ( ($series_num > 0) && ($show_name_only == 0) ) {
+			$output .= "#" . $series_num . " of ";
+		}
+		$output .= "<a href='/series/" . $this->seoize($series_data['id'], $series_data['series_name']) . "'>" . $series_data['series_name'] . "</a>";
+		return $output;
+	}
+
 
 	public function formatMoney($amount=0.00) {
 		setlocale(LC_ALL, ''); // Locale will be different on each system.
